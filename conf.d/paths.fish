8bbdfbb82128c0c5df77d78d3ce8568e63f02425
Jorge Bucaran <j@bucaran.me>
2016-05-20 22:58:28 +0900

    No variable expansion. Close #2.

--- conf.d/paths.fish
+++ conf.d/paths.fish
@@ -12,49 +12,40 @@ if test -z "$paths_config"
     end
 end
 
-################### Remove after 2.3.0 is ubiquitous ###################
 switch "$FISH_VERSION"
+################### TO BE DEPRECATED ###################
     case 2.2.0
-        set -l real_home ~
-
-        function -S _paths_replace_tilde
-            printf "%s\n" $argv | sed "s,~,$real_home,g"
-        end
-
         for file in "$paths_config"/*
             if test -d "$file"
                 set -l name (command basename "$file")
 
                 for file in "$file"/*
                     read -laz values < $file
-                    set values (_paths_replace_tilde $values)
-                    set -gx "$name" $$name $values
+                    set -gx $name $$name $values
                 end
 
             else if test -f "$file"
                 set -l name (command basename "$file")
                 read -laz values < $file
-                set values (_paths_replace_tilde $values)
-                set -gx "$name" $values
+                set -gx $name $values
             end
         end
+################### TO BE DEPRECATED ###################
 
-        exit
-end
-################### Remove after 2.3.0 is ubiquitous ###################
+    case \*
+        for file in "$paths_config"/*
+            if test -d "$file"
+                set -l name (string split -rm1 / "$file")[-1]
 
-for file in "$paths_config"/*
-    if test -d "$file"
-        set -l name (string split -rm1 / "$file")[-1]
+                for file in "$file"/*
+                    read -laz values < $file
+                    set -gx $name $$name $values
+                end
 
-        for file in "$file"/*
-            read -laz values < $file
-            echo "set -gx $name \$$name $values"
+            else if test -f "$file"
+                set -l name (string split -rm1 / "$file")[-1]
+                read -laz values < $file
+                set -gx $name $values
+            end
         end
-
-    else if test -f "$file"
-        set -l name (string split -rm1 / "$file")[-1]
-        read -laz values < $file
-        echo "set -gx $name $values"
-    end
-end | source
+end
